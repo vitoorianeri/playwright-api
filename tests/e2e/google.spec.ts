63f7e5a65c3cc1b7062c1d1dc461dcb73dcca1c5
Vitoria Neri <vitoria.vneri@gmail.com>
2025-05-22 16:24:38 -0300

    adjust in the regex

--- tests/e2e/google.spec.ts
+++ tests/e2e/google.spec.ts
@@ -5,21 +5,14 @@ import fs from 'fs';
 test('search for image', async ({ page }) => {
   const google = new Google(page);
 
-  await page.route('**/*', (route, request) => {
-
-    if (route.request().resourceType() === 'image') {
-      const url = route.request().url();
-
-      if (request.url().includes('images?')) {
-        console.log(url);
-        return route.fulfill({
-          status: 400,
-          contentType: 'text/plain',
+  await page.route(/https:\/\/encrypted-tbn0\.gstatic\.com\/images\?q=tbn:ANd9Gc.*/, (route, request) => {
+    const url = route.request().url();
+    console.log('Interceptado:', url);
+    return route.fulfill({
+      status: 400,
+      contentType: 'text/plain',
         });
-      }
-    }
-
-    route.continue();
+      
   })
 
   await google.navegatTo();
@@ -36,22 +29,15 @@ test('replace image', async ({ page }) => {
 
   const imageBuffer = fs.readFileSync('C:/Users/vickv/OneDrive/Imagens/20230709_111722.jpg');
 
-  await page.route('**/*', (route, request) => {
-
-    if (route.request().resourceType() === 'image') {
-      const url = route.request().url();
-
-      if (request.url().includes('images?')) {
-        console.log(url);
-        return route.fulfill({
-          status: 200,
-          contentType: 'image/jpg',
-          body: imageBuffer,
+  await page.route(/https:\/\/encrypted-tbn0\.gstatic\.com\/images\?q=tbn:ANd9Gc.*/, (route, request) => {
+    const url = route.request().url();
+    console.log('Interceptado:', url);
+    return route.fulfill({
+      status: 200,
+      contentType: 'image/jpg',
+      body: imageBuffer,
         });
-      }
-    }
-
-    route.continue();
+      
   })
 
   await google.navegatTo();
